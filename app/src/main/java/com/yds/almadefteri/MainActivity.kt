d50ba2fa2728c9dd3d47f99caddb4457991a7e54
Eozturkbu <48592441+Eozturkbu@users.noreply.github.com>
2026-06-09 16:43:54 +0300

    Add files via upload

---
+++ app/src/main/java/com/yds/almadefteri/MainActivity.kt
@@ -0,0 +1,47 @@
+SSSSpackage com.yds.almadefteri
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.yds.almadefteri.ui.theme.ÇalışmaDefteriTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            ÇalışmaDefteriTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    ÇalışmaDefteriTheme {
+        Greeting("Android")
+    }
+}
